29df5734c1fc62ff8634157e757af1d71ae0a14d
Shane <86496244+Shanep98@users.noreply.github.com>
2021-10-07 16:53:10 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+# import models.py
+# main menu a, v, b
+# Cleanup
+# V- Display a produce by id
+# A- Add a new produce
+# B- backing up the Database 
+# loop running
---
+++ models.py
@@ -0,0 +1,4 @@
+# #create a database
+# inventory.db
+# create a Module
+# product_id, product_name,product_price,product_quantity,date_updated
